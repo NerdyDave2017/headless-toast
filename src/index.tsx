8f6104b3389ca49a18aac6c7296a9a418dff664f
NerdyDave2017 <nerdydave2017@gmail.com>
2025-01-30 13:52:40 +0100

    feat: add forward ref to Toaster

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import react, { useState, useEffect } from "react";
+import react, { useState, useEffect, forwardRef } from "react";
 import { ToastState, toast } from "./state";
 import { ToasterProps, ToastProps } from "./types";
 import { ToastT } from "./types";
@@ -7,28 +7,32 @@ const cn = (...classes: (string | undefined)[]) => {
   classes.filter(Boolean).join(" ");
 };
 
-const Toaster = ({ duration, position }: ToasterProps) => {
-  const [toasts, setToasts] = useState<ToastT[]>([]);
+const Toaster = forwardRef<HTMLElement, ToastProps>(
+  ({ duration, position }, ref) => {
+    const [toasts, setToasts] = useState<ToastT[]>([]);
 
-  useEffect(() => {
-    ToastState.subscribe((toast) => {
-      setToasts((prevToasts) => [...prevToasts, toast]);
-    });
-  }, []);
+    useEffect(() => {
+      ToastState.subscribe((toast) => {
+        setToasts((prevToasts) => [...prevToasts, toast]);
+      });
+    }, []);
 
-  return (
-    <ol>
-      {toasts.map((toast, index) => (
-        <Toast
-          key={index}
-          element={toast.element}
-          duration={toast.duration ? toast.duration : duration}
-          position={toast.position ? toast.position : position}
-        />
-      ))}
-    </ol>
-  );
-};
+    return (
+      <section ref={ref}>
+        <ol>
+          {toasts.map((toast, index) => (
+            <Toast
+              key={index}
+              element={toast.element}
+              duration={toast.duration ? toast.duration : duration}
+              position={toast.position ? toast.position : position}
+            />
+          ))}
+        </ol>
+      </section>
+    );
+  }
+);
 
 const Toast = ({ duration, position, element }: ToastProps) => {
   return <li>{element}</li>;
--- test/src/app/page.tsx
+++ test/src/app/page.tsx
@@ -1,7 +1,28 @@
+"use client";
 import Image from "next/image";
 import { Toaster, toast } from "headless-toast";
 
 export default function Home() {
+  // setTimeout(() => {
+  //   toast({
+  //     element: (
+  //       <div className="w-[220px] h-[50px] bg-rose-400 text-white">
+  //         This is a div
+  //       </div>
+  //     ),
+  //   });
+  // }, 3000);
+
+  const handleToast = () => {
+    toast({
+      element: (
+        <div className="w-[220px] h-[50px] bg-rose-400 text-white p-3">
+          This is a div
+        </div>
+      ),
+    });
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -25,6 +46,13 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <p
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            rel="noopener noreferrer"
+            onClick={handleToast}
+          >
+            Test Toast
+          </p>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -40,6 +68,7 @@ export default function Home() {
             />
             Deploy now
           </a>
+
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -97,6 +126,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <Toaster />
     </div>
   );
 }
