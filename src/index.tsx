5cfbde57677651ac07d2fd3a6cfda7a094abdabf
NerdyDave2017 <nerdydave2017@gmail.com>
2025-02-03 19:48:04 +0100

    feat: add css for toast and toaster

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,29 @@ import react, { useState, useEffect, forwardRef, useMemo, useRef } from "react";
 import { ToastState, toast } from "./state";
 import { Position, ToasterProps, ToastProps } from "./types";
 import { ToastT } from "./types";
+import "./styles.css";
+
+// Viewport padding
+const VIEWPORT_OFFSET = "32px";
+
+// Mobile viewport padding
+const MOBILE_VIEWPORT_OFFSET = "16px";
+
+// Default gap between toasts
+const GAP = 14;
 
 const cn = (...classes: (string | undefined)[]) => {
   classes.filter(Boolean).join(" ");
 };
 
 const Toaster = forwardRef<HTMLElement, ToasterProps>((props, ref) => {
-  const { position = "bottom-right", duration, gap } = props;
+  const {
+    position = "bottom-right",
+    duration,
+    gap = GAP,
+    mobileOffset,
+    offset,
+  } = props;
 
   const [toasts, setToasts] = useState<ToastT[]>([]);
   const possiblePositions = useMemo(() => {
@@ -31,9 +47,48 @@ const Toaster = forwardRef<HTMLElement, ToasterProps>((props, ref) => {
     });
   }, []);
 
+  function assignOffset(
+    defaultOffset: ToasterProps["offset"],
+    mobileOffset: ToasterProps["mobileOffset"]
+  ) {
+    const styles = {} as React.CSSProperties;
+
+    [defaultOffset, mobileOffset].forEach((offset, index) => {
+      const isMobile = index === 1;
+      const prefix = isMobile ? "--mobile-offset" : "--offset";
+      const defaultValue = isMobile ? MOBILE_VIEWPORT_OFFSET : VIEWPORT_OFFSET;
+
+      function assignAll(offset: string | number) {
+        ["top", "right", "bottom", "left"].forEach((key) => {
+          styles[`${prefix}-${key}`] =
+            typeof offset === "number" ? `${offset}px` : offset;
+        });
+      }
+
+      if (typeof offset === "number" || typeof offset === "string") {
+        assignAll(offset);
+      } else if (typeof offset === "object") {
+        ["top", "right", "bottom", "left"].forEach((key) => {
+          if (offset[key] === undefined) {
+            styles[`${prefix}-${key}`] = defaultValue;
+          } else {
+            styles[`${prefix}-${key}`] =
+              typeof offset[key] === "number"
+                ? `${offset[key]}px`
+                : offset[key];
+          }
+        });
+      } else {
+        assignAll(defaultValue);
+      }
+    });
+
+    return styles;
+  }
+
   return (
     <section ref={ref}>
-      {possiblePositions.map((position, index) => {
+      {possiblePositions.map((position) => {
         const [y, x] = position.split("-");
 
         if (!toasts.length) return null;
@@ -45,6 +100,12 @@ const Toaster = forwardRef<HTMLElement, ToasterProps>((props, ref) => {
             data-headless-toaster
             data-y-position={y}
             data-x-position={x}
+            style={
+              {
+                ...assignOffset(offset, mobileOffset),
+                "--gap": `${gap}px`,
+              } as React.CSSProperties
+            }
           >
             {toasts.map((toast, index) => (
               <Toast
@@ -62,7 +123,12 @@ const Toaster = forwardRef<HTMLElement, ToasterProps>((props, ref) => {
 });
 
 const Toast = ({ duration, position, element }: ToastProps) => {
-  return <li data-headless-toast>{element}</li>;
+  const [y, x] = position!.split("-");
+  return (
+    <li data-headless-toast data-y-position={y} data-x-position={x}>
+      {element}
+    </li>
+  );
 };
 
 export { toast, Toaster, type ToastT, type ToasterProps };
--- src/types.ts
+++ src/types.ts
@@ -15,12 +15,24 @@ export type Position =
   | "top-center"
   | "bottom-center";
 
+type Offset =
+  | {
+      top?: string | number;
+      right?: string | number;
+      bottom?: string | number;
+      left?: string | number;
+    }
+  | string
+  | number;
+
 export type Element = ReactElement | ReactHTMLElement<any>;
 
 export type ToasterProps = {
   position?: Position;
   duration?: number;
   gap?: number; // space between toasts
+  offset?: Offset;
+  mobileOffset?: Offset;
 };
 
 export type ToastProps = {
